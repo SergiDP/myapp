8b3a21a1f092edbad9aa4b5e0950245abe91ae8b
SergiDP <sergi.delgadopuig@gmail.com>
2020-01-18 17:05:59 +0100

    change port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,11 +11,11 @@ module.exports = {
       script    : './server.js',
       env: {
         NODE_ENV: 'development',
-        PORT: '3000'
+        PORT: '8000'
       },
       env_production : {
         NODE_ENV: 'production',
-        PORT: '3000'
+        PORT: '8000'
       }
     }
   ]
